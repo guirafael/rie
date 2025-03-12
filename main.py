b18d57470c8fc44dc13930878bf519bbb1db4cdb
guirafael <guilhermerafael347@gmail.com>
2025-03-11 22:02:10 -0300

    acessando arquivo xls

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import pandas as pd
+
+# selecionando os dados a partir do seu cabecalho
+df = pd.read_excel("snx690_20250228132802.xls", header=7)
+
+# convertendo a string em data e hora
+df["Time"] = pd.to_datetime(df["Time"])
+
+# separando a hora em uma nova coluna
+df["Hour"] = df["Time"].dt.hour
+
+# imprimindo apenas "Time" e "Hour"
+print(df[["Time", "Hour"]].head())
